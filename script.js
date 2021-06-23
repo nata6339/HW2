11cf1ba7d380966b8bff399101cb164ab6fc9a1c
Nataliia <nata.buchna@gmail.com>
2021-06-23 22:29:16 +0300

    step 4/5

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+console.log('------------- # 4')
+
+let a = 10,
+    b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+console.log('------------- # 5')
+
+let arr = [0, 1, 2, 3, 4, 5];
+for (let i=1; i < arr.length; i++)
+
+console.log(arr[i]);
